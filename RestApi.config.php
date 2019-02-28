f2e851faf4b89449bca496d6bea20ee85dc0f6ee
Thomas Aull <post@thomasaull.de>
2019-02-28 15:03:49 +0100

    Fix: Allow 0-9 for endpoint url

--- RestApi.config.php
+++ RestApi.config.php
@@ -30,10 +30,10 @@ $config = array(
     'type' => 'text',
     'label' => 'API Endpoint',
     'description' => "Endpoint under which your API should be available", 
-    'pattern' => '[a-z-/]+',
+    'pattern' => '[a-z0-9-/]+',
     'minlength' => 1,
     'required' => true, 
     'value' => 'api',
-    'notes' => "('a-z', '-' and '/' allowed, Default: 'api')\nFor subdirectories use e.g. subdir/api (no leading slash)"
+    'notes' => "('a-z', 0-9, '-' and '/' allowed, Default: 'api')\nFor subdirectories use e.g. subdir/api (no leading slash)"
   )
 );
