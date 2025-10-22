ee458fc9e24efb32894f8bf6598d6287c324968f
Sebastian Schendel <sebi@sebi.dev>
2025-10-23 00:44:52 +0200

    Bump version

--- AppApi.module.php
+++ AppApi.module.php
@@ -24,7 +24,7 @@ class AppApi extends Process implements Module {
 		return [
 			'title' => 'AppApi',
 			'summary' => 'Module to create a REST API with ProcessWire',
-			'version' => '1.3.7',
+			'version' => '1.4.0',
 			'author' => 'Sebastian Schendel',
 			'icon' => 'terminal',
 			'href' => 'https://modules.processwire.com/modules/app-api/',
